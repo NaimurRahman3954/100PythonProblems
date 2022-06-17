01ac6ba9717a6a90f10555a1571b680e5b27451f
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-06-18 01:02:45 +0600

    Create 1.1_convert_input.py

---
+++ 1.1_convert_input.py
@@ -0,0 +1,12 @@
+# The problem
+# Take two inputs from the user. One will be an integer. The other will be a float number. Then multiply them to display the output.
+
+# Hints
+# Use input. By default, input gives you a string. Then use int and float to convert the input to a number. And then multiply them. That’s it.
+
+# Solution
+print("Enter an integer: ")
+int_input = int(input())
+print("Enter a float: ")
+float_input = float(input())
+print(int_input * float_input)
