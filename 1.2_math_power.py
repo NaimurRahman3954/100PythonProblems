784f4d4e8bd46cb6486f095b819bb1b4de8b2c97
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-06-18 01:08:10 +0600

    Create 1.2_math_power.py

---
+++ 1.2_math_power.py
@@ -0,0 +1,12 @@
+# The problem
+# Take two numbers from the users. Calculate the result of second number power of the first number.
+
+# Hints
+# To power, you will need to use two asterisks symbols (two multiplication symbols). For example 4 to the power 3 will be
+
+# Solution
+print("Enter a number: ")
+number = int(input())
+print("Enter a power: ")
+power = int(input())
+print(number ** power)
