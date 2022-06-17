614d4dae380c528a42cb4a71245183481a03b326
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-06-18 01:31:33 +0600

    Update 1.3_random_number.py

--- 1.3_random_number.py
+++ 1.3_random_number.py
@@ -1,8 +1,10 @@
-# The problem
-# Create a random number between 0 to 10
+'''
+The problem
+Create a random number between 0 to 10
 
-# Hints
-# To create a random number, you have to import a built-in library named random. And then you can call the randint method on it
+Hints
+To create a random number, you have to import a built-in library named random. And then you can call the randint method on it
+'''
 
 # Solution
 import random
