7a112d29eab12201a496f328603ec835dad4cf12
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-06-18 01:29:26 +0600

    Update 1.4_floor_division.py

--- 1.4_floor_division.py
+++ 1.4_floor_division.py
@@ -1,11 +1,14 @@
-# The problem
-# Find the floor division of two numbers.
+import math
 
-# Hints
-# Floor division means the integer part of a division operation. For example, if you divide 17/5 the quotient will be 3.4. Here the integer part is 3. So, you have to find the integer part of the division operation.
+'''
+The problem
+Find the floor division of two numbers.
+
+Hints
+Floor division means the integer part of a division operation. For example, if you divide 17/5 the quotient will be 3.4. Here the integer part is 3. So, you have to find the integer part of the division operation.
+'''
 
 # Solution
-import math
 print(17 // 5)
 # used two division operators to find the floor division of 17/5
 
